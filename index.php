799b056145b6f0d1a83dea2f40d2e3d1c9122b50
Jonathan Henrique de Araújo Silva <jonathandearaujosilva@gmail.com>
2018-02-15 23:51:42 -0300

    improve index.php comment

--- index.php
+++ index.php
@@ -7,7 +7,10 @@
     <div id="react-container"></div>
     <script type="text/javascript" src="Index.bundled.js"></script>
     <script type="text/javascript">
-    /*mount the props as you want. Its probably compatible with technologies such as Java .jsp, Rails .erb, Python .jinja...*/
+    /*
+      mount the props as you want. Its probably compatible with technologies such as Java .jsp, Rails .erb, Python .jinja.
+      All you have to do is serealize some data and pass to props as a JSON object
+    */
       var props = <?php echo json_encode(array('value' =>rand())) ?>;
       Global.Index.render(props, 'react-container');
     </script>
